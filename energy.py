a59ceda1b10f58418d05c958189a028251092e29
Jin Hyun Chang <jinhyun.chang@gmail.com>
2023-08-11 14:39:55 +0200

    Update energy.py

--- energy.py
+++ energy.py
@@ -20,10 +20,14 @@ import json
 import random
 from clease.montecarlo import MCEvaluator
 from ase.calculators.emt import EMT
+
+
 def load_json_file(file_path):#Load the ECI values from the JSON file
     with open(file_path, 'r') as file:
         json_data = json.load(file)
     return json_data 
+
+
 def run_mc(cell,T):#Run the Monte Carlo Simulation for the cell 
 	internal_energy=[]
 	mc=Montecarlo(cell,T)
@@ -34,9 +38,14 @@ def run_mc(cell,T):#Run the Monte Carlo Simulation for the cell
 	mean_energy.append(np.mean(internal_energy/tot))
 	formula.append(cell.get_chemical_formula(empirical=False))
 	temperature.append(T)
-def find_pure_energy(atoms, string,T):#Finding the pure energy of the lattices
+
+
+def find_pure_energy(atoms, string, T):
+"""
+Find pure energy of the lattice
+"""
 	for i in range(len(atoms)):
-			atoms[i].symbol=string
+		atoms[i].symbol=string
 	atoms = attach_calculator(settings, atoms=atoms, eci=eci)
 	formula.append(atoms.get_chemical_formula(empirical=False))
 	mean_energy.append(atoms.get_total_energy()/tot)
@@ -47,13 +56,16 @@ def find_pure_energy(atoms, string,T):#Finding the pure energy of the lattices
 	else:
 		Au_conc.append(0)
 		return(atoms.get_total_energy()/tot)
-def get_free_energy(U,T):
+
+
+def get_free_energy(U, T):
 	Kb=1.380649e-23
 	beta=1/(Kb*T)
 	N=1000
 #	Z=math.exp(-U*beta*N)
 	F=(Kb*T*U*beta*N)
-	free_energy.append(F)
+	return F
+
 file_path = 'eci_l1.json'  
 json_dict = load_json_file(file_path)
 eci=json_dict
@@ -84,18 +96,25 @@ for T in temp:#for different temperature ranges
 	Au_pure=find_pure_energy(atoms,'Au',T)	
 	while(i>=1 and i<=len(atoms)-1):
 		cell = connect('aucu.db').get(id=1).toatoms()*size
-		for j in range (0,i):
+		for j in range (0, i):
 			cell[j].symbol='Cu'
 		cell = attach_calculator(settings, atoms=cell, eci=eci)
 		run_mc(cell,T)
 		i=i+10
 	Cu_pure=find_pure_energy(atoms,'Cu',T)
+
 for i in range(len(mean_energy)):
 	u=((mean_energy[i]-(Au_conc[i]*Au_pure)-((1-Au_conc[i])*Cu_pure))*96.484934)
 	formation.append(u)
-	get_free_energy(mean_energy[i],temperature[i])
-for i in range (len(free_energy)):
-	free_energy[i]=(free_energy[i]-(Au_conc[i]*free_energy[0])-((1-Au_conc[i])*free_energy[-1]))*96.484934
+	free_energy.append(get_free_energy(mean_energy[i], temperature[i]))
+
+# for i in range(len(free_energy)):
+# 	free_energy[i]=(free_energy[i]-(Au_conc[i]*free_energy[0])-((1-Au_conc[i])*free_energy[-1]))*96.484934
+
+for energy, i in enumerate(free_energy):
+	energy = energy - (Au_conc[i] * free_energy[0]) - ((1 - Au_conc[i]) * free_energy[-1])
+	energy *= 96.484934
+
 plt.plot(Au_conc,formation)
 plt.xlabel('Au Conc')
 plt.ylabel('Formation Energy (kJ/mole)')
