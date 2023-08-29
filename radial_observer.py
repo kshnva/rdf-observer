0e5bcb21460430f3627e768052115521daa36d88
kushhhhs <kushnavas@egmail.com>
2023-08-29 22:46:25 +0530

    Rectify the Radial Distribution Function Observer

--- radial_observer.py
+++ radial_observer.py
@@ -1,27 +1,66 @@
-from clease.datastructures import MCStep, SystemChanges
+from clease.datastructures import MCStep
 from clease.montecarlo.observers import MCObserver
-from ase.geometry import get_layers
-from ase.db import connect
-from clease.tools import update_db
-from clease.calculator import attach_calculator
-from clease.montecarlo import Montecarlo
-from clease.settings import Concentration,CEBulk
 import ase
 from clease.datastructures import MCStep
-import json 
 import numpy as np
 import matplotlib.pyplot as plt 
-from ase.geometry.analysis import Analysis
-def get_radial(atoms):
-    ana=Analysis(atoms)
-    rad=ana.get_rdf(rmax=6.50,nbins=100,elements=None, return_dists=True)
-    x=rad[0][1]
-    y=rad[0][0]
-    print(y)    
-    plt.plot(x,y, color='blue', linewidth=1)
-    plt.xlabel('r / Å',size=15)
-    plt.ylabel('g(r)',size=15)
-    plt.savefig(fname="Radial Distribution Function")
+
+def get_atom_positions(atom_positions,atomic_number,atoms):
+
+    '''
+        Gets the positions of the atoms whose RDF is to be calculated
+    '''
+    
+    for i in range(len(atoms)):
+        if (atoms.numbers[i] in atomic_number):#Check if the element is desired in the RDF to be produced
+            atom_positions.append(i)
+    
+    return atom_positions
+
+
+def get_data(atom_positions,frequency,bins,atoms):
+
+    '''
+        Gets the No. of atoms between a particular range of distance from the reference atom
+    '''
+
+    dist=atoms.get_distances(0,atom_positions,vector=False)
+
+    for d in dist:
+        for i,bin in enumerate(bins):
+            if(d<=bin):
+                frequency[i]+=1
+                break
+
+    return(frequency)
+
+
+def save_plot_graph(frequency,bins):
+
+    '''
+        Plots RDF from the reference atom
+    '''
+   
+    plt.plot(bins,frequency, color='blue', linewidth=1)
+    plt.xlabel('Distance (in Å)',size=15)
+    plt.ylabel('No.of Atoms ',size=15)
+    plt.title("Radial Distribution Function")
+    plt.savefig("Radial Distribution Function")
+
+
+def get_radial(atoms,number_of_bins=100,max_distance=6.50):
+
+    elements=[29,79]#Atomic Number of Elements to be included in the RDF
+    bins=np.linspace(0,max_distance,number_of_bins)
+    bins=np.round(bins,2)
+    frequency=[0]*number_of_bins
+    atom_positions=[]
+    atom_positions=get_atom_positions(atom_positions=atom_positions,atomic_number=elements,atoms=atoms)
+    frequency=get_data(atom_positions=atom_positions,frequency=frequency,bins=bins,atoms=atoms)
+    save_plot_graph(frequency=frequency,bins=bins)
+    
+           
+
 class RDFObserver(MCObserver):
 
 
@@ -35,46 +74,15 @@ class RDFObserver(MCObserver):
         self.lowest_energy = np.inf
 
 
-        
     def observe_step(self, mc_step: MCStep) -> None:
         """
         Check if the current state has lower energy and store the current
-        state if it has a lower energy than the previous state.
+        state if it has a lower energy than the previous state then get the 
+        RDF for the Atoms Object .
 
-        mc_step: MCStep
-             Instance of MCStep with information on the latest step.
         """
         if mc_step.energy < self.lowest_energy:
 
             self.lowest_energy = mc_step.energy
             self.emin_atoms.numbers = self.atoms.numbers
             get_radial(self.atoms)
-                
-    
-
-def load_json_file(file_path):#Load the ECI values from the JSON file
-    with open(file_path, 'r') as file:
-        json_data = json.load(file)
-    return json_data 
-file_path = 'rdf-observer/eci_l1.json'  
-eci = load_json_file(file_path)
-conc=Concentration(basis_elements=[['Au','Cu']])
-conc.set_conc_formula_unit(formulas=["Au<x>Cu<1-x>"], variable_range={"x": (0, 1)})
-settings=CEBulk(crystalstructure='fcc',
-               a=3.8,
-               supercell_factor=27,
-               concentration=conc,
-               db_name="aucu.db",
-               max_cluster_dia=[6.0,5,5])
-
-size=(6,6,6)
-tot=np.product(size)
-T=500
-atoms = connect('aucu.db').get(id=1).toatoms()*size
-atoms = attach_calculator(settings, atoms=atoms, eci=eci)
-for j in range (0,int(tot/3)):
-	atoms[j].symbol='Cu'
-monitor = RDFObserver(atoms)
-mc = Montecarlo(atoms, T)
-mc.attach(monitor, interval=50)
-mc.run(steps=500)
